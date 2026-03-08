7716e3c83b9937ec0cc26eccf4e02ff7700e74f5
SaltDel <salmabankole@gmail.com>
2026-03-08 14:29:28 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include "database.h"
+
+int main() {
+    char line[256];
+
+    db_init();
+
+    while (fgets(line, sizeof(line), stdin)) {
+
+        char command[10];
+        char key[100];
+        char value[100];
+
+        if (sscanf(line, "%s", command) != 1)
+            continue;
+
+        if (strcmp(command, "SET") == 0) {
+
+            if (sscanf(line, "%s %s %s", command, key, value) == 3) {
+                db_set(key, value);
+            }
+
+        } else if (strcmp(command, "GET") == 0) {
+
+            if (sscanf(line, "%s %s", command, key) == 2) {
+                char *result = db_get(key);
+
+                if (result)
+                    printf("%s\n", result);
+                else
+                    printf("NULL\n");
+            }
+
+        } else if (strcmp(command, "EXIT") == 0) {
+
+            break;
+
+        }
+    }
+
+    return 0;
+}
